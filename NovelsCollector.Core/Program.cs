d29d865a7a7444ffc0ccb0691414c3fba2ce6fef
git03-Nguyen <nguyendinhanhvlqt@gmail.com>
2024-05-30 19:12:30 +0700

    Add temporary CORS: localhost:3000 for frontend testing

--- NovelsCollector.Core/Program.cs
+++ NovelsCollector.Core/Program.cs
@@ -9,6 +9,18 @@ builder.Services.AddPluginManager();
 //builder.Services.Configure<PluginsDbSettings>(builder.Configuration.GetSection("PluginsDatabase"));
 //builder.Services.AddSingleton<PluginsService>();
 
+// Add CORS for frontend: http://localhost:3000         TODO: move to config
+var corsName = "Frontend";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(corsName, builder =>
+    {
+        builder.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -23,6 +35,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UsePluginManager();
+
+app.UseCors(corsName);
 
 app.UseHttpsRedirection();
 
