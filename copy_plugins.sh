9cc417ef6e58784690e66ceaf397770f151f0659
git03-Nguyen <nguyendinhanhvlqt@gmail.com>
2024-06-03 00:59:21 +0700

    Create bash script: copy_plugins.sh for automatically copy plugins

---
+++ copy_plugins.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# How to run: bash copy_plugins.sh
+
+# Define the source and destination directories
+declare -A dirs=(
+    ["./NovelsCollector.Plugins/Source.DTruyenCom/bin/Debug/net8.0/"]="./NovelsCollector.Core/bin/Debug/net8.0/Plugins/DTruyenCom"
+    ["./NovelsCollector.Plugins/Source.SSTruyenVn/bin/Debug/net8.0/"]="./NovelsCollector.Core/bin/Debug/net8.0/Plugins/SSTruyenVn"
+    ["./NovelsCollector.Plugins/Source.TruyenFullVn/bin/Debug/net8.0/"]="./NovelsCollector.Core/bin/Debug/net8.0/Plugins/TruyenFullVn"
+    ["./NovelsCollector.Plugins/Source.TruyenTangThuVienVn/bin/Debug/net8.0/"]="./NovelsCollector.Core/bin/Debug/net8.0/Plugins/TruyenTangThuVienVn"
+)
+
+# Loop through each source and destination pair
+for src in "${!dirs[@]}"; do
+    dest=${dirs[$src]}
+    
+    # Create the destination directory if it does not exist
+    mkdir -p "$dest"
+    
+    # Copy all files from the source to the destination
+    cp -r "$src"* "$dest"
+    
+    # Print a message indicating the operation was successful
+    echo "Copied files from $src to $dest"
+done
